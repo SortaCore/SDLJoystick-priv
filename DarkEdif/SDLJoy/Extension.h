b33e9142a2b4be2f795bcc454ec52ff56e5dc13b
Phi <phi@dark-wire.com>
2020-11-04 02:54:21 +0000

    Full commit
    Cloned from original repo release v1.12:
    https://github.com/PiKeyAr/SDLJoystick/commit/919867c29e9bc2205cd36627fdf0929357c0c896
    And DarkEdif SDK v6:
    https://github.com/SortaCore/MMF2Exts/commit/dcd86fd42fd80e74a0190d99a84aba71aad489b2
    
    And SDL2 2.0.12.

---
+++ DarkEdif/SDLJoy/Extension.h
@@ -0,0 +1,157 @@
+#include <DarkEdif.h>
+
+#define SDL_MAIN_HANDLED
+#include "sdl/SDL_config.h"
+#include "sdl/SDL.h"
+#include "sdl/SDL_main.h"
+#include "sdl/SDL_joystick.h"
+#include "sdl/SDL_haptic.h"
+
+#pragma comment (lib, "Setupapi.lib")
+#pragma comment (lib, "Version.lib")
+#pragma comment (lib, "Imm32.lib")
+
+#ifdef _DEBUG
+#pragma comment (lib, "../Lib/Windows/sdl2d.lib")
+#pragma comment (lib, "../Lib/Windows/sdl2maind.lib")
+#else
+#pragma comment (lib, "../Lib/Windows/sdl2.lib")
+#pragma comment (lib, "../Lib/Windows/sdl2main.lib")
+#endif
+
+struct SDL_JoystickData
+{
+	SDL_Joystick * joystick;
+	SDL_Haptic * haptic;
+	bool connected;
+	int joy_id;
+	Sint16 axis[8];
+	Uint8 held_buttons[32];
+	Uint8 held_buttons_last[32];
+	Uint8 hat[4];
+	int ball_x[4];
+	int ball_y[4];
+	Sint8 currentheld[32];
+	Sint8 lastpressed;
+	Sint8 lastreleased;
+	char num_buttons;
+	char num_axes;
+	char num_hats;
+	char num_balls;
+};
+
+
+class Extension
+{
+public:
+
+	RUNDATA * rdPtr;
+	RunHeader *	rhPtr;
+
+	Edif::Runtime Runtime;
+
+	static const int MinimumBuild = 254;
+	static const int Version = 1;
+
+	static const OEFLAGS OEFLAGS = OEFLAGS::NEVER_SLEEP;
+	static const OEPREFS OEPREFS = OEPREFS::NONE;
+	
+	static const int WindowProcPriority = 100;
+
+	Extension(RUNDATA * rdPtr, EDITDATA * edPtr, CreateObjectInfo * cobPtr);
+	~Extension();
+
+	// To add items to the Fusion Debugger, just uncomment this line.
+	DarkEdif::FusionDebugger FusionDebugger;
+	// After enabling it, you run FusionDebugger.AddItemToDebugger() inside Extension's constructor
+	// As an example:
+	std::tstring exampleDebuggerTextItem;
+
+	/*  Add any data you want to store in your extension to this class
+		(eg. what you'd normally store in rdPtr in old SDKs).
+
+		Unlike rdPtr, you can store real C++ objects with constructors
+		and destructors, without having to call them manually or store
+		a pointer.
+	*/
+
+
+	constexpr static int NUM_HATS = 4;
+	constexpr static int NUM_AXES = 8;
+	constexpr static int NUM_BALLS = 8;
+	constexpr static int NUM_DEVICES = 16;
+	constexpr static int NUM_BUTTONS = 32;
+
+	SDL_JoystickData SDL_Data[NUM_DEVICES];
+	bool DeviceIDOK(int devID);
+	bool ButtonIDOK(int buttonID);
+	bool HatIDOK(int devID);
+	bool AxisIDOK(int axisID);
+	bool BallIDOK(int ballID);
+
+	// int MyVariable;
+
+
+
+
+	/*  Add your actions, conditions and expressions as real class member
+		functions here. The arguments (and return type for expressions) must
+		match EXACTLY what you defined in the JSON.
+
+		Remember to link the actions, conditions and expressions to their
+		numeric IDs in the class constructor (Extension.cpp)
+	*/
+
+	/// Actions
+
+	/// Conditions
+
+		bool IsConnected(int devID);
+		bool ButtonPressed(int devID, int buttonID);
+		bool ButtonHeldDown(int devID, int buttonID);
+		bool ButtonReleased(int devID, int buttonID);
+		bool AnyButtonPressed(int devID);
+		bool AnyButtonHeldDown(int devID);
+		bool AnyButtonReleased(int devID);
+
+	/// Expressions
+		
+		int GetAxis(int joy, int axis);
+		int GetButton(int joy, int button);
+		int GetHat(int joy, int hat);
+		int GetBallXDelta(int joy, int ball);
+		int GetBallYDelta(int joy, int ball);
+		int NumAxes(int joy);
+		int NumButtons(int joy);
+		int NumHats(int joy);
+		int NumBalls(int joy);
+		int NumJoysticks();
+		const TCHAR * GetDeviceGUID(int joy);
+		const TCHAR * GetDeviceName(int joy);
+		const TCHAR * GetButtonsHeld(int joy);
+		int HeldButtonIndex(int joy, int index);
+		int LastButtonPressedIndex(int joy);
+		int LastButtonReleasedIndex(int joy);
+
+
+
+	/* These are called if there's no function linked to an ID */
+
+	void Action(int ID, RUNDATA * rdPtr, long param1, long param2);
+	long Condition(int ID, RUNDATA * rdPtr, long param1, long param2);
+	long Expression(int ID, RUNDATA * rdPtr, long param);
+
+
+
+
+	/*  These replace the functions like HandleRunObject that used to be
+		implemented in Runtime.cpp. They work exactly the same, but they're
+		inside the extension class.
+	*/
+
+	REFLAG Handle();
+	REFLAG Display();
+
+	short Pause();
+	short Continue();
+};
---
+++ README.md
@@ -0,0 +1,47 @@
+# SDL Joystick Object by PkR
+
+SDL Joystick is an extension for Clickteam Fusion 2.5 that makes use of the [SDL2 Joystick API](https://www.libsdl.org/release/SDL-1.2.15/docs/html/joystick.html) to interface with game controllers.
+
+This is a DarkEdif port that does not require Visual Studio Runtime Redistributable, or SDL.dll.
+
+This build instead makes use of a static build of SDL, limited to just including SDL_JOYSTICK, SDL_HAPSTIC (and from necessity as DirectInput expects a video window handle, SDL_VIDEO).
+
+SDL Joystick uses the SDL2 library, and you must conform to its [license terms](https://www.libsdl.org/license.php) in all projects using this extension.
+
+### To update SDL
+The basic process to update SDL is:
+1. Download source from [here](https://www.libsdl.org/download-2.0.php), extract it.
+  **WARNING:** Do not have spaces in your any folder of your source or build path. CMake will not work with it and cause compiling issues later. You cannot move the source folder without deleting CMake cache and redoing Steps 3-6.
+2. Run CMake GUI on the folder with source, then press Configure.
+  Don't forget to set a build location: I set mine to just `source folder\build`.
+3. It will ask you what platform and toolset to use
+  Under platform, pick `Win32`
+  Under toolset, put `v141_xp` for XP+ compatibility, otherwise leave blank for Vista+.
+  ![options](https://cdn.discordapp.com/attachments/409878770242289666/773008157361307648/unknown.png)
+4. Press OK, it'll load the list of values that you see above.
+* Check the Grouped and Advanced checkboxes above the list.
+* Under CMAKE, change any "/MDd" to "/MTd", which will make SDL not depend on VS redistributable; likewise, all "/MD" to "/MT". This switches Runtime Library.
+  For XP compatibility, under CMAKE_C_FLAGS and CMAKE_CXX_FLAGS, add a `/DWINVER=0x0501 /D_WIN32_WINNT=0x0501`.
+  ![define example](https://cdn.discordapp.com/attachments/409878770242289666/773008280145625138/unknown.png)
+* Under RENDER category, enable SOL_STATIC, SDL_JOYSTICK, SDL_HAPTIC, SDL_VIDEO, SDL_LOADSO. Disable the rest.
+* Under SDL category, enable SOL_STATIC, SDL_JOYSTICK, SDL_HAPTIC, SDL_VIDEO, SDL_LOADSO. Disable the rest.
+* Under VIDEO category, only enable VIDEO_DUMMY.
+* Under Ungrouped, enable LIBC.
+5. Press Configure again, if no error, then press Generate.
+6. Press Open Project button.
+7. In the project, find "SDL_dynapi.h", and set `#define SDL_DYNAMIC_API` to 0 at the bottom of the file.
+  (This file will have a lot of warnings about not doing a static build due to difficulty upgrading, but they don't apply to Fusion users, only to more lowlevel developers.)
+8. In the project, go to "build\include\SDL_config.h", and where `#define HAS_WCSLCPY 1` and `#define HAS_WCSLCAT 1` are, **comment them out**.
+  (These are mistakenly detected and will cause linker issues)
+9. You're done; press to build Debug and Release configurations.
+  (If they do not build and say files are missing, you didn't read the warning in step 1.)
+
+
+The resulting files:
+* Copy include files from source\include -> repo\DarkEdif\Inc\Shared\SDL.
+* Replace source\build\include\SDL_config.h -> repo\DarkEdif\Inc\Shared\SDL\SDL_config.h.
+* Copy lib files from source\build\Debug -> repo\DarkEdif\Lib\Windows
+* Copy lib files from source\build\Release -> repo\DarkEdif\Lib\Windows
+10. All done. Next build of the extension will have the latest SDL2.
+
+Note that Extension.h tags multiple more OS libraries in `#pragma comment (lib, "x.lib")` that SDL desires.
